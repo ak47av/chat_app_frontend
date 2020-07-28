738e3f815d89fe55eef4f6a5e3c9cd910047178c
Arun Vijay <sarunkarthikeyancgmhss@gmail.com>
2020-07-28 16:59:44 +0530

    Updates

---
+++ precache-manifest.54ae09bb9321b7d238dcd7ccf0a1d6e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b261d1edc10048ba58a2a2a89a2dfc",
+    "url": "/chat_app_frontend/index.html"
+  },
+  {
+    "revision": "7bba3c8d9d5afa47c03c",
+    "url": "/chat_app_frontend/static/css/2.664f7800.chunk.css"
+  },
+  {
+    "revision": "9dfeda262eb137a45d0a",
+    "url": "/chat_app_frontend/static/css/main.b364870a.chunk.css"
+  },
+  {
+    "revision": "7bba3c8d9d5afa47c03c",
+    "url": "/chat_app_frontend/static/js/2.c921a3bd.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/chat_app_frontend/static/js/2.c921a3bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dfeda262eb137a45d0a",
+    "url": "/chat_app_frontend/static/js/main.55561b09.chunk.js"
+  },
+  {
+    "revision": "03d6cbf286a7259cabde",
+    "url": "/chat_app_frontend/static/js/runtime-main.88d15f22.js"
+  },
+  {
+    "revision": "8e8fe54ab32c1eb7715790ca3e76c3e4",
+    "url": "/chat_app_frontend/static/media/download.8e8fe54a.jfif"
+  },
+  {
+    "revision": "03b270704bc5e52a87ebc9de5bc9781c",
+    "url": "/chat_app_frontend/static/media/sea.03b27070.jpg"
+  }
+]);
